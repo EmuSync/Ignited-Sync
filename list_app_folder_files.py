ba54a19573749ffc04c13b34397dfeaf955fd33e
Nythepegasus <me@nythepegas.us>
2023-08-22 08:37:40 -0400

    Get token refreshing working

--- list_app_folder_files.py
+++ list_app_folder_files.py
@@ -1,11 +1,35 @@
 import requests
 
 # Access token we can intercept from Delta
-# TODO: Figure out how to use refresh_token to automatically refresh access_token
-with open(".env", "r") as file:
-    conf = {line.split("=")[0]: line.split("=")[1] for line in file.read().split("\n") if line != ''}
+def read_conf():
+    with open(".env", "r") as file:
+        return {line.split("=")[0]: line.split("=")[1] for line in file.read().split("\n") if line != ''}
+
+def write_conf(conf):
+    with open(".env", "w") as file:
+        for i, v in conf.items():
+            file.write(f"{i}={v}\n")
+
+conf = read_conf()
 
 s = requests.Session()
+
+def refresh_token(conf):
+    data = {
+        'refresh_token': conf['REFRESH_TOKEN'],
+        'emm_support': 1,
+        'device_os': 'iOS 17.0',
+        'client_id': conf['CLIENT_ID'],
+        'gpsdk': 'gid-5.0.2',
+        'grant_type': 'refresh_token'
+     }
+    refdata = s.post('https://oauth2.googleapis.com/token', data=data).json()
+    conf['ACCESS_TOKEN'] = refdata['access_token']
+    conf['ID_TOKEN'] = refdata['id_token']
+    conf['TYPE'] = refdata['token_type']
+    conf['SCOPE'] = refdata['scope']
+    write_conf(conf)
+    return conf
 
 API_URL = 'https://www.googleapis.com/drive/v3/files'
 
